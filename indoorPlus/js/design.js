4f39799ed872d126653f065cc526b6eb08d5689e
ttubin <ttubiny@gmail.com>
2022-03-31 13:01:23 +0900

    3차 모바일 작업 js 파일 수정

--- indoorPlus/js/design.js
+++ indoorPlus/js/design.js
@@ -1,23 +1,28 @@
 const partnerSwiper = new Swiper('.swiper.partner-slider', {
-    loop: true, 
-    speed: 1000, 
-    navigation: {
+    loop: true, // 무한루프 (마지막 슬라이더에서 처음 슬라이더랑 이어지게 연결)
+    autoplay: { // 자동재생 delay 로 원하는 초 설정 (1s = 1000)
+        delay: 3000
+    },
+    speed: 1000, // 슬라이더 넘어가는 속도
+    navigation: { // 슬라이더 왼쪽, 오른쪽 버튼
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     }
 });
 
 const numberSwiper = new Swiper('.swiper.numbers-slider', {
-    loop: true, 
-    speed: 1000, 
-    centeredSlides: true,
-    breakpoints: {
-        375: {
+    loop: true, // 무한루프 (마지막 슬라이더에서 처음 슬라이더랑 이어지게 연결)
+    autoplay: { // 자동재생 delay 로 원하는 초 설정 (1s = 1000)
+        delay: 3000
+    },
+    speed: 1000, // 슬라이더 넘어가는 속도
+    centeredSlides: true, // 슬라이더 가운데 설정
+    breakpoints: { // 반응형일 때, 슬라이더 갯수 달라지게 설정
+        375: { // 모바일
             slidesPerView: 1
         },
-        
-        1920: {
-            slidesPerView: 3,
-        },
+        1920: { // 웹
+            slidesPerView: 3
+        }
     }
 });
